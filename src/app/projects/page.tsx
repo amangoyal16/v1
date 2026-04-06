529694234fd9c33b13760b55907c988f233539b9
amangoyal16 <amangoyal676@gmail.com>
2026-04-06 19:59:50 +0530

    refactor: add whitespace to projects page and remove yarn.lock file

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,7 @@ import ProjectsList from '@/components/ProjectsList';
 import Footer from '@/components/Footer';
 
 export default function ProjectsPage() {
+
   return (
     <div className="min-h-screen mx-auto bg-white">
       <ProjectsList />
